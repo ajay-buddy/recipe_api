be8cfe39040023814be7702176a1361607fc7da3
Ajay Yadav <ajay.yadav@buddy4study.com>
2019-12-01 01:31:06 +0530

    Implemented core app

---
+++ app/core/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+
+class UserManger(BaseUserManager):
+
+    def create_user(self, email, password=None, **extra_feilds):
+        """Create and Saves a new User"""
+
+        user = self.model(email=email, **extra_feilds)
+        user.set_password(password)
+
+        user.save(using=self.db)
+        return user
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """Custom user model that supports useing email instead of username"""
+
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManger()
+    USERNAME_FIELD = 'email'
